b1dbf744e6e0f3736a056c579be36ac630247537
Little-Jelly <207986989@qq.com>
2020-02-18 22:49:47 +0800

    Mybatis简单实现增删改查四个功能

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # SSM_Base_MyBatic
+
+
+完成了myBatis的基础的增删改查
+
+query： 根据条件查询一条数据
+queryFuzzy： 根据条件，进行模糊查询多条数据
+add： 插入一条数据
+update： 更新一条数据
+delete： 删除一条数据
+
+涉及使用到的Bean：
+org.apache.ibatis.io.Resource
+java.io.InputStream
+org.apache.ibatis.session.SqlSessionFactory
+org.apache.ibatis.session.SqlSessionFactoryBuilder
+org.apache.ibatis.session.SqlSession
+
+设计使用到的Bean的方法：
+Resources.getResourceAsStream(String a)
+SqlSessionFactoryBuilder().build(InputStream inputStream)
+SqlSessionFactory.openSession()
+SqlSession.selectOne()
+SqlSession.selectList()
+SqlSession.insert()
+SqlSession.update()
+SqlSession.delete()
+SqlSession.commit()
+SqlSession.close()
---
+++ src/Main.java
@@ -0,0 +1,128 @@
+import com.yyf.mybatis.example.query.POJO.Customer;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("hello, world");
+    }
+
+    /**
+     * 根据id查询表数据
+     * 查询结果是一条数据
+     * res: successfully
+     * @throws IOException
+     */
+    @Test
+    public void test_findCustomerByIdTest() throws IOException {
+        String resource = "com/yyf/mybatis/example/query/mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        Customer customer = sqlSession.selectOne("com.yyf.mybatis.example.query.mapper.CustomerMapper.findCustomerId", 1);
+
+        System.out.println(customer.toString());
+        sqlSession.close();
+    }
+
+    /**
+     * 使用模糊查询查询表数据
+     * 查询结果是多条数据
+     * res:successfully
+     * @throws IOException
+     */
+    @Test
+    public void test_findCustomerId_fuzzy() throws IOException{
+        String resource = "com/yyf/mybatis/example/queryFuzzy/mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        List<com.yyf.mybatis.example.queryFuzzy.POJO.Customer> customers = sqlSession.selectList("com.yyf.mybatis.example.queryFuzzy.mapper.CustomerMapper.findCustomerId_fuzzy", "j");
+        for(com.yyf.mybatis.example.queryFuzzy.POJO.Customer customer : customers ){
+            System.out.println(customer);
+        }
+        sqlSession.close();
+    }
+
+    /**
+     * 向数据库表中插入一条数据
+     *
+     * @throws IOException
+     */
+    @Test
+    public void test_addCustomer() throws IOException{
+        String resource = "com/yyf/mybatis/example/add/mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        com.yyf.mybatis.example.add.POJO.Customer customer = new com.yyf.mybatis.example.add.POJO.Customer();
+//        customer.setId(4);
+        customer.setUsername("peter");
+        customer.setJobs("student");
+        customer.setPhone("13333533092");
+        System.out.println(customer);
+        int rows = sqlSession.insert("com.yyf.mybatis.example.add.mapper.CustomerMapper.addCustomer",customer);
+        if (rows > 0) {
+            System.out.println("您成功插入了："+rows+"条数据！");
+        }else {
+            System.out.println("执行插入操作失败！！！");
+        }
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    /**
+     * 向数据库表中更新一条数据
+     *
+     * @throws IOException
+     */
+    @Test
+    public void test_updateCustomer() throws IOException{
+        String resource = "com/yyf/mybatis/example/update/mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession =sqlSessionFactory.openSession();
+        com.yyf.mybatis.example.update.POJO.Customer customer = new com.yyf.mybatis.example.update.POJO.Customer();
+        customer.setId(7);
+        customer.setUsername("rose");
+        customer.setJobs("programmer");
+        customer.setPhone("13111111144");
+        int rows = sqlSession.update("com.yyf.mybatis.example.update.mapper.CustomerMapper.updateCustomer", customer);
+        if(rows > 0){
+            System.out.println("您成功修改了" + rows + "条数据！");
+        }else {
+            System.out.println("执行修改操作失败！！！");
+        }
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    /**
+     * 删除数据库表中的一条数据
+     *
+     * @throws IOException
+     */
+    @Test
+    public void test_deleteCustomer() throws IOException {
+        String resource = "com/yyf/mybatis/example/delete/mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        int rows = sqlSession.delete("com.yyf.mybatis.example.delete.mapper.CustomerMapper.deleteCustomer",4);
+        if(rows > 0){
+            System.out.println("您成功删除了" + rows + "条数据！");
+        }else {
+            System.out.println("执行删除操作失败！！！");
+        }
+        sqlSession.commit();
+        sqlSession.close();
+    }
+}
